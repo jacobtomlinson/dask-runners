0e4d3d3cc209cb754739b29ca1920d2044ca2ab6
Lehman Garrison <lgarrison@flatironinstitute.org>
2024-05-02 05:18:19 -0400

    `SlurmRunner` proof-of-concept (#3)
    * slurm: initial proof-of-concept
    
    Use `SLURM_PROCID` to get the rank, and `scheduler_file` to get the scheduler address. The `SlurmRunner` doesn't actually need to know the scheduler address, it just needs to pass along the scheduler file.
    
    * base: allow Runners to not have the scheduler address. This lets the SlurmRunner just pass along the scheduler_file rather than opening it.
    
    * slurm: add test
    
    * slurm: wait for scheduler file to exist before launching the client/workers. Also append the job ID to the filename stem.
    
    * slurm: store the scheduler_address in the SlurmRunner for compatibility with Client
    
    * slurm: smarter default scheduler_file name

---
+++ dask_hpc_runner/tests/slurm_core_context.py
@@ -0,0 +1,7 @@
+from dask.distributed import Client
+from dask_hpc_runner import  SlurmRunner
+
+with SlurmRunner() as runner:
+    with Client(runner) as client:
+        assert client.submit(lambda x: x + 1, 10).result() == 11
+        assert client.submit(lambda x: x + 1, 20, workers=2).result() == 21
