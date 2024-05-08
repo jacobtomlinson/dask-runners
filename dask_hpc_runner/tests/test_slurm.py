0a493b322f6aa97a162abca813b99da47247df42
Jacob Tomlinson <jtomlinson@nvidia.com>
2024-05-08 12:04:29 +0100

    Ensure tests use a single node

--- dask_hpc_runner/tests/test_slurm.py
+++ dask_hpc_runner/tests/test_slurm.py
@@ -9,7 +9,7 @@ import pytest
 def test_context(srun):
     script_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "slurm_core_context.py")
 
-    p = subprocess.Popen(srun + ["-n", "4", sys.executable, script_file])
+    p = subprocess.Popen(srun + ["-n", "4", "--ntasks-per-node", "4", sys.executable, script_file])
 
     p.communicate()
     assert p.returncode == 0
@@ -19,7 +19,9 @@ def test_context(srun):
 def test_small_world(srun):
     script_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "slurm_core_context.py")
 
-    p = subprocess.Popen(srun + ["-n", "1", sys.executable, script_file], stderr=subprocess.PIPE)
+    p = subprocess.Popen(
+        srun + ["-n", "1", "--ntasks-per-node", "1", sys.executable, script_file], stderr=subprocess.PIPE
+    )
 
     _, std_err = p.communicate()
     assert p.returncode != 0
